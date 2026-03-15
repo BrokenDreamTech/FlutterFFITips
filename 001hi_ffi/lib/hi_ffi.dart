a3c7ef9d8811e76a3e8e3b4db8f4a19e9d80beb6
BrokenDreamTech <3278005235@qq.com>
2026-03-15 14:04:01 +0800

    chore(project): 初始化 Flutter FFI 插件项目结构
    - 添加 Android、iOS、Linux、macOS、Windows 和鸿蒙平台的 gitignore 配置文件

---
+++ 001hi_ffi/lib/hi_ffi.dart
@@ -0,0 +1,107 @@
+
+import 'dart:async';
+import 'dart:ffi';
+import 'dart:io';
+import 'dart:isolate';
+
+import 'hi_ffi_bindings_generated.dart';
+
+
+int sum(int a, int b) => _bindings.sum(a, b);
+
+Future<int> sumAsync(int a, int b) async {
+  final SendPort helperIsolateSendPort = await _helperIsolateSendPort;
+  final int requestId = _nextSumRequestId++;
+  final _SumRequest request = _SumRequest(requestId, a, b);
+  final Completer<int> completer = Completer<int>();
+  _sumRequests[requestId] = completer;
+  helperIsolateSendPort.send(request);
+  return completer.future;
+}
+
+const String _libName = 'hi_ffi';
+
+/// The dynamic library in which the symbols for [HiFfiBindings] can be found.
+final DynamicLibrary _dylib = () {
+  if (Platform.isMacOS || Platform.isIOS) {
+    return DynamicLibrary.open('$_libName.framework/$_libName');
+  }
+  if (Platform.isAndroid || Platform.isLinux || Platform.operatingSystem == 'ohos') {
+    return DynamicLibrary.open('lib$_libName.so');
+  }
+  if (Platform.isWindows) {
+    return DynamicLibrary.open('$_libName.dll');
+  }
+  throw UnsupportedError('Unknown platform: ${Platform.operatingSystem}');
+}();
+
+/// The bindings to the native functions in [_dylib].
+final HiFfiBindings _bindings = HiFfiBindings(_dylib);
+
+
+
+class _SumRequest {
+  final int id;
+  final int a;
+  final int b;
+
+  const _SumRequest(this.id, this.a, this.b);
+}
+
+
+class _SumResponse {
+  final int id;
+  final int result;
+
+  const _SumResponse(this.id, this.result);
+}
+
+/// Counter to identify [_SumRequest]s and [_SumResponse]s.
+int _nextSumRequestId = 0;
+
+/// Mapping from [_SumRequest] `id`s to the completers corresponding to the correct future of the pending request.
+final Map<int, Completer<int>> _sumRequests = <int, Completer<int>>{};
+
+
+Future<SendPort> _helperIsolateSendPort = () async {
+
+  final Completer<SendPort> completer = Completer<SendPort>();
+
+  final ReceivePort receivePort = ReceivePort()
+    ..listen((dynamic data) {
+      if (data is SendPort) {
+        completer.complete(data);
+        return;
+      }
+      if (data is _SumResponse) {
+        // The helper isolate sent us a response to a request we sent.
+        final Completer<int> completer = _sumRequests[data.id]!;
+        _sumRequests.remove(data.id);
+        completer.complete(data.result);
+        return;
+      }
+      throw UnsupportedError('Unsupported message type: ${data.runtimeType}');
+    });
+
+  // Start the helper isolate.
+  await Isolate.spawn((SendPort sendPort) async {
+    final ReceivePort helperReceivePort = ReceivePort()
+      ..listen((dynamic data) {
+        // On the helper isolate listen to requests and respond to them.
+        if (data is _SumRequest) {
+          final int result = _bindings.sum_long_running(data.a, data.b);
+          final _SumResponse response = _SumResponse(data.id, result);
+          sendPort.send(response);
+          return;
+        }
+        throw UnsupportedError('Unsupported message type: ${data.runtimeType}');
+      });
+
+    // Send the port to the main isolate on which we can receive requests.
+    sendPort.send(helperReceivePort.sendPort);
+  }, receivePort.sendPort);
+
+  // Wait until the helper isolate has sent us back the SendPort on which we
+  // can start sending requests.
+  return completer.future;
+}();
---
+++ Readme.md
@@ -0,0 +1,82 @@
+### 🟢🟢 点个关注不迷路🟢🟢  (≧◡≦) ♡
+#### 🥺拜托了欧尼酱，阿里嘎多  Thank you  高马斯密达💖
+### 👇👇⬇️⬇️
+
+[![哔哩哔哩: 断梦科技 (shields.io)](https://img.shields.io/badge/哔哩哔哩-断梦科技-FF3F7B)](https://space.bilibili.com/3546827542497753) [![CSDN: 断梦科技 (shields.io)](https://img.shields.io/badge/CSDN-断梦科技-FC5531)](https://blog.csdn.net/2402_88197112) [![Github: BrokenDreamTech (shields.io)](https://img.shields.io/badge/Github-BrokenDreamTech-black)](https://github.com/BrokenDreamTech) [![Gitcode: BrokenDreamTech (shields.io)](https://img.shields.io/badge/Gitcode-BrokenDreamTech-DA203E)](https://gitcode.com/BrokenDreamTech)
+
+| 哔哩哔哩                                              | CSDN                                          | Github                                       | Gitcode                                       |
+| ----------------------------------------------------- | --------------------------------------------- | -------------------------------------------- | --------------------------------------------- |
+| [传送门](https://space.bilibili.com/3546827542497753) | [传送门](https://blog.csdn.net/2402_88197112) | [传送门](https://github.com/BrokenDreamTech) | [传送门](https://gitcode.com/BrokenDreamTech) |
+
+> 一般CSDN会第一时间更新图文教程，后续会上传源码到Github和Gitcode上，最后再录视频教程发到哔哩哔哩，如果没时间的话可能不会录视频。
+>
+
+# Flutter FFI Tips
+
+#### 介绍：如题
+
+
+
+## 效果图片
+
+#### 🟢温馨提示：如果图片无法加载请前往国内Gitcode镜像仓库
+
+![image01](./ReadmeRes/image01.png)
+
+![image01](./ReadmeRes/image02.png)
+
+## 代码地址
+
+🟢温馨提示：如果图片无法加载请前往国内Gitcode镜像仓库
+
+Github：[https://github.com/BrokenDreamTech/FlutterFFITips](https://github.com/BrokenDreamTech/FlutterFFITips)
+
+Gitcode：[https://gitcode.com/BrokenDreamTech/FlutterFFITips](https://gitcode.com/BrokenDreamTech/FlutterFFITips)
+
+
+
+## 环境
+
+Android Studio 2025.3.2
+
+Flutter 3.35.8-ohos-0.0.2
+
+## 目录
+
+#### 视频导航
+
+1. 待定
+
+#### 图文目录
+
+1. [ffi官方插件模板](001hi_ffi/README.md)
+
+2. 待定
+
+
+#### 常用命令
+
+```sh
+xmake f -p windows -a x64
+
+xmake f -p harmony --sdk=D:/sofe/devecostudio/sdk/default/openharmony/native --bin=D:/sofe/devecostudio/sdk/default/openharmony/native/llvm/bin
+
+
+flutter create --template=plugin_ffi --platforms=android,ios,windows,macos,linux,ohos hi_ffi
+
+flutter build windows
+
+flutter build apk --release --split-per-abi
+
+flutter build app --release
+```
+
+
+
+## 鸣谢
+
+
+
+## 📄 License
+
+#### [Apache License 2.0](./LICENSE.txt)
